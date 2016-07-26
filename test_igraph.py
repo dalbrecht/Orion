6fc6f7071968b4903e9870c0235eb7faa5f73f07
dalbrecht <donald.albrecht@nielsen.com>
2016-07-26 16:15:32 -0500

    Adding hint

--- test_igraph.py
+++ test_igraph.py
@@ -4,3 +4,5 @@ import igraph.test
 def test_igraph():
     assert igraph.test.run_tests(verbosity=255) is None
 
+if __name__ == "__main__":
+    print("No tests were executed.  Did you mean to use nose or pytest?")
